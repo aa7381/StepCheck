672fee8b36e0e1ae30568c1c8f3426a0093a747b
Adir <aa7381@bs.amalnet.k12.il>
2025-12-20 18:29:47 +0200

    4

--- app/src/main/java/com/example/stepcheck/SettingsFragment.java
+++ app/src/main/java/com/example/stepcheck/SettingsFragment.java
@@ -1,9 +1,15 @@
 package com.example.stepcheck;
 
+import static android.content.Context.MODE_PRIVATE;
+import static com.example.stepcheck.FBRef.refAuth;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,17 +21,23 @@ import androidx.fragment.app.Fragment;
  */
 public class SettingsFragment extends Fragment {
 
+    Button myButton;
+
+
     /**
      * Called to have the fragment instantiate its user interface view.
      *
-     * @param inflater The LayoutInflater object that can be used to inflate any views in the fragment.
-     * @param container If non-null, this is the parent view that the fragment's UI should be attached to.
+     * @param inflater           The LayoutInflater object that can be used to inflate any views in the fragment.
+     * @param container          If non-null, this is the parent view that the fragment's UI should be attached to.
      * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state.
      * @return Return the View for the fragment's UI.
      */
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
+
 }
+
